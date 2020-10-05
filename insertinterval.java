17db38d23bc3397f1d69631bb7f523493fa67a3b
Kevin Dai <kevin.dai.2451@gmail.com>
2020-10-04 20:28:22 -0400

    added java solution

---
+++ insertinterval.java
@@ -0,0 +1,28 @@
+//Java Solution
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int index = 0;
+        
+        while (index < intervals.length && intervals[index][1] < newInterval[0]) {
+            result.add(intervals[index]);
+            index++;
+        }
+        
+        while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
+            index++;
+        }
+        
+        result.add(newInterval);
+        
+        while (index < intervals.length) {
+            result.add(intervals[index]);
+            index++;
+        }
+        
+        return result.toArray(new int[result.size()][2]);
+    }
+}
